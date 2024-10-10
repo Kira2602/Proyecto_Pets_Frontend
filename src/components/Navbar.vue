03d74889f4bdb069012f326825002de9892cbbbf
Rosario Calisaya <misa26amane@gmail.com>
2024-10-10 02:46:56 -0400

    [IMP] implementando vista pagina de inicio y agregando componente navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,104 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">
+      <img src="@/components/images/nav_bar_logo.png" alt="Logo" class="logo-img" />
+    </div>
+    <div class="menu-buttons">
+      <ul class="menu">
+        <a href="#" class="menu-link">Inicio</a>
+        <a href="#" class="menu-link">Funcionalidades</a>
+        <a href="#" class="menu-link">Redes Sociales</a>
+      </ul>
+      <div class="buttons">
+        <button class="btn btn-outline">Iniciar Sesión</button>
+        <button class="btn btn-filled">Registrarse</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #fff;
+  border-radius: 50px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  margin: 20px;
+}
+
+.logo-img {
+  width: 50px;
+  height: 50px;
+}
+
+.menu-buttons {
+  display: flex;
+  align-items: center;
+}
+
+.menu {
+  display: flex;
+  gap: 20px;
+  margin-right: 40px;
+}
+
+.menu-link {
+  text-decoration: none;
+  font-size: 16px;
+  color: #45413e;
+  font-weight: 500;
+  transition: color 0.3s ease;
+}
+
+.menu-link:hover {
+  color: #db7f67;
+}
+
+/* Estilo de los botones */
+.buttons {
+  display: flex;
+  gap: 10px;
+}
+
+.btn {
+  padding: 10px 20px;
+  border-radius: 30px;
+  font-size: 14px;
+  font-weight: 600;
+  border: none;
+  cursor: pointer;
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
+}
+
+.btn-outline {
+  background-color: transparent;
+  color: #db7f67;
+  border: 2px solid #db7f67;
+}
+
+.btn-outline:hover {
+  background-color: #db7f67;
+  color: white;
+}
+
+.btn-filled {
+  background-color: #db7f67;
+  color: white;
+}
+
+.btn-filled:hover {
+  background-color: #f4cfc6;
+  color: #db7f67;
+}
+</style>
---
+++ src/views/PaginaInicio.vue
@@ -0,0 +1,577 @@
+<template>
+  <div>
+    <header>
+      <Navbar />
+    </header>
+
+    <!-- Primera sección (Encabezado con gato y texto) -->
+    <section class="hero">
+      <div class="hero-content">
+        <h1 id="typing-effect"></h1>
+
+        <button class="hero-button">Registra tu mascota</button>
+      </div>
+      <div class="hero-image">
+        <img src="@/components/images/logo_encabezado.png" alt="Cute Cat" />
+      </div>
+    </section>
+
+    <!-- Segunda sección (Nuestras funcionalidades principales) -->
+    <section>
+      <h2>Nuestras funcionalidades principales</h2>
+      <ul class="shop-pets">
+        <li class="card-large card-light" id="sup-dog">
+          <div class="card-image">
+            <img src="@/components/images/gestion_citas_medicas_icon.png" />
+          </div>
+          <ul>
+            Gestión de citas médicas y recordatorios
+            <li><a href="#">Food &amp; Treats</a></li>
+            <button class="btn btn-outline">Ir</button>
+          </ul>
+        </li>
+
+        <li class="card-large card-dark" id="sup-cat">
+          <div class="card-image">
+            <img src="@/components/images/collar.png" />
+          </div>
+          <ul>
+            Planificación de actividades diarias
+            <li><a href="#">Food &amp; Treats</a></li>
+            <button class="btn btn-outline">Ir</button>
+          </ul>
+        </li>
+
+        <li class="card-large card-dark" id="sup-bird">
+          <div class="card-image">
+            <img src="@/components/images/history.png" />
+          </div>
+          <ul>
+            Historial de actividades diarias
+            <li><a href="#">Food &amp; Treats</a></li>
+            <button class="btn btn-outline">Ir</button>
+          </ul>
+        </li>
+        <li class="card-large card-light" id="sup-fish">
+          <div class="card-image">
+            <img src="@/components/images/reminder.png" />
+          </div>
+          <ul>
+            Notificaciones automáticas
+            <li><a href="#">Food</a></li>
+            <button class="btn btn-outline">Ir</button>
+          </ul>
+        </li>
+      </ul>
+    </section>
+
+    <!-- Sección de Redes Sociales -->
+    <section class="social-media">
+      <h2>Visita nuestras redes sociales</h2>
+      <div class="social-icons">
+        <a href="#"><img src="@/components/images/facebook_icon.png" alt="Facebook" /></a>
+        <a href="#"><img src="@/components/images/youtube_icon.png" alt="YouTube" /></a>
+        <a href="#"><img src="@/components/images/instagram_icon.png" alt="Instagram" /></a>
+      </div>
+    </section>
+
+    <!-- Tercera sección (Footer) -->
+    <footer class="footer">
+      <div class="footer-content">
+        <p>© 2024 Grupo PetCare. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'inicio',
+  components: {
+    Navbar
+  },
+  mounted() {
+    const text = `Organización total para el\n bienestar de tus mascotas!`
+    this.typeEffect(text, 'typing-effect', 100)
+  },
+  methods: {
+    typeEffect(text, elementId, speed) {
+      const element = document.getElementById(elementId)
+      let index = 0
+      const typingInterval = setInterval(() => {
+        // Reemplaza los saltos de línea con <br/>
+        element.innerHTML += text.charAt(index) === '\n' ? '<br/>' : text.charAt(index)
+        index++
+        if (index === text.length) {
+          clearInterval(typingInterval)
+        }
+      }, speed)
+    }
+  }
+}
+</script>
+
+<style scoped>
+:root {
+  --text-01: #9d8189;
+  --light-01: #f9f9f9;
+  --light-02: #ffffff;
+  --brand-01: #db7f67;
+  --brand-02: #ffcad4;
+  --card-hover: 0px 4px 24px rgba(0, 0, 0, 0.15);
+  --card-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
+  --hover-timing: all 0.2s ease;
+  --nav-card-size: 240px;
+}
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  transition: var(--hover-timing);
+
+  background: var(--light-01);
+
+  width: 100vw;
+  overflow-x: hidden;
+}
+
+#typing-text {
+  font-size: 2rem;
+  font-family: 'Poppins', sans-serif;
+  color: #7a6652;
+  white-space: pre-line;
+  overflow: hidden;
+  border-right: 0.15em solid #7a6652;
+  letter-spacing: 0.15em;
+  animation:
+    typing 3.5s steps(40, end),
+    blink-caret 0.75s step-end infinite;
+}
+
+/* Animación del typing */
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+}
+
+/* Parpadeo del cursor */
+@keyframes blink-caret {
+  from,
+  to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: #7a6652;
+  }
+}
+
+/* Estilos de la primera sección */
+.hero {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #ffcad4;
+  padding: 40px;
+  margin: 20px 0;
+  position: relative;
+}
+
+.hero-content {
+  flex: 1;
+  max-width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.hero h1 {
+  font-size: 3.5rem;
+  color: #7a6652;
+  margin-bottom: 20px;
+  text-align: left;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.hero-button {
+  padding: 15px 40px;
+  background-color: #9d8189;
+  color: white;
+  border: none;
+  border-radius: 30px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  margin-top: 20px;
+}
+
+.hero-button:hover {
+  background-color: #ffe5d9;
+  color: #0a0909;
+}
+
+.hero-image {
+  position: absolute;
+  right: 290px;
+  bottom: 0;
+  width: 300px;
+}
+
+.hero-image img {
+  width: 500px;
+  height: auto;
+}
+
+/* Ajustes para pantallas pequeñas */
+@media only screen and (max-width: 768px) {
+  .hero {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+  }
+
+  .hero-content {
+    max-width: 100%;
+  }
+
+  .hero h1 {
+    font-size: 1.5rem;
+  }
+
+  .hero-button {
+    font-size: 1rem;
+    padding: 10px 30px;
+  }
+
+  .hero-image {
+    position: static;
+    width: 250px;
+  }
+}
+
+/* Estilos de la sección de redes sociales */
+.social-media {
+  background-color: #ffcad4;
+  padding: 40px 0;
+  text-align: center;
+  margin-bottom: 0;
+}
+
+.social-media h2 {
+  font-size: 1.8rem;
+  color: #7a6652;
+  margin-bottom: 20px;
+}
+
+.social-icons {
+  display: flex;
+  justify-content: center;
+  gap: 100px;
+}
+
+.social-icons a img {
+  width: 90px;
+  height: 90px;
+  object-fit: contain;
+  border-radius: 15%;
+  transition: transform 0.3s ease;
+}
+
+.social-icons a img:hover {
+  transform: scale(1.1);
+}
+
+/* Footer */
+.footer {
+  background-color: #9d8189;
+  padding: 40px 20px;
+  text-align: center;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  margin-top: 0;
+}
+
+.footer-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1rem;
+  gap: 10px;
+}
+
+.footer-content p {
+  margin: 5px 0;
+}
+
+.card-large {
+  flex: 1;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  transition: var(--hover-timing);
+  cursor: pointer;
+}
+
+.card-large {
+  box-shadow: var(--card-shadow);
+  border-radius: 12px;
+  overflow: hidden;
+}
+
+.card-large:hover {
+  box-shadow: var(--card-hover);
+  transform: scale(1.01);
+}
+
+.card-image {
+  transform: scale(1.01);
+}
+
+.card-image {
+  width: 100%;
+  aspect-ratio: 1/1;
+  position: relative;
+  border-radius: 12px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: var(--hover-timing);
+}
+
+.card-large > .card-image {
+  aspect-ratio: 3/2;
+}
+
+.card-image > img {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  object-fit: contain;
+}
+
+.card-large a {
+  text-decoration: none;
+  font-weight: 600;
+}
+
+.card-large > .card-image > a {
+  position: absolute;
+  bottom: 0;
+  z-index: 1;
+  width: 100%;
+  font-size: 24px;
+  line-height: 1.2;
+}
+
+.card-large > ul {
+  display: flex;
+  flex-direction: column;
+  grid-gap: 8px;
+  flex: 1;
+  color: var(--text-01);
+  padding: 0 40px 40px;
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 1.5;
+}
+
+.card-large > ul > li a {
+  line-height: 32px;
+  font-size: 14px;
+}
+
+.card-large#sup-dog,
+.card-med#sup-dog > .card-image {
+  background: #9d8189;
+}
+
+.card-large#sup-cat,
+.card-med#sup-cat > .card-image {
+  background: #ffcad4;
+}
+
+.card-large#sup-bird,
+.card-med#sup-bird > .card-image {
+  background: #ffe5d9;
+}
+
+.card-large#sup-fish,
+.card-med#sup-fish > .card-image {
+  background: #d8e2dc;
+}
+
+.card-large.card-dark a,
+.card-large.card-dark > ul {
+  color: var(--text-01);
+}
+
+.card-large.card-light a,
+.card-large.card-light > ul {
+  color: var(--light-01);
+}
+
+button {
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 24px;
+  padding: 12px 24px;
+  border-radius: 48px;
+  display: flex;
+  flex-direction: row;
+  grid-gap: 8px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+footer {
+  position: relative;
+  width: 100%;
+  padding: 0 var(--pg-margin);
+}
+
+.btn-group {
+  display: flex;
+  flex-direction: row;
+  grid-gap: 16px;
+}
+
+section h2 {
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 1.2;
+  text-align: center;
+  color: #9d8189;
+  margin-bottom: 32px;
+}
+
+.card-image img {
+  width: 80%;
+  height: 80%;
+}
+
+.card-large > ul > li:last-of-type {
+  margin-bottom: 40px;
+}
+
+.card-large button {
+  margin-top: auto;
+}
+
+.btn-group {
+  display: flex;
+  flex-direction: row;
+  grid-gap: 16px;
+}
+
+footer {
+  background: var(--text-01);
+  padding: 80px 80px 0px;
+  margin-top: 80px;
+  display: flex;
+}
+
+/* Estilos de los botones "Ir" de las cards*/
+.btn {
+  padding: 10px 0px;
+  border-radius: 30px;
+  font-size: 20px;
+  font-weight: 600;
+  border: none;
+  cursor: pointer;
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
+}
+
+.btn-outline {
+  background-color: #ffffff;
+  color: #9d8189;
+}
+
+.btn-outline:hover {
+  background-color: #f4acb7;
+  color: white;
+}
+
+@media only screen and (min-width: 1200px) {
+  :root {
+    --pg-margin: 48px;
+  }
+
+  section.hero h1 {
+    --hero-text: 48px;
+  }
+
+  section.hero {
+    aspect-ratio: 2/1;
+  }
+
+  .btn-group {
+    flex-direction: row;
+  }
+
+  .shop-pets {
+    display: flex;
+    flex-direction: row;
+    grid-gap: 24px;
+    margin-bottom: 40px;
+    margin-left: 20px;
+    margin-right: 20px;
+    justify-content: space-between;
+  }
+
+  footer {
+    flex-direction: row;
+    text-align: left;
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  :root {
+    --pg-margin: 80px;
+  }
+
+  section.hero h1 {
+    --hero-text: 56px;
+  }
+
+  section.hero {
+    aspect-ratio: 3/1;
+    background-size: 30%;
+    background-position: 90% 60%;
+  }
+
+  .shop-pets {
+    display: flex;
+    flex-direction: row;
+    grid-gap: 24px;
+    margin-bottom: 40px;
+    margin-left: 20px;
+    margin-right: 20px;
+    justify-content: space-between;
+  }
+
+  footer {
+    flex-direction: row;
+    text-align: left;
+  }
+}
+</style>
